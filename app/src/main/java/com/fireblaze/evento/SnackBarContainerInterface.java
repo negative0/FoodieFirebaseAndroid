d67261e486511e89a74c82377d19860b7f5cd0ee
Chaitanya <bchaitanya15@gmail.com>
2016-10-18 23:40:07 +0530

    Added Interface for snackbar getContainer()

--- app/src/main/java/com/fireblaze/evento/SnackBarContainerInterface.java
+++ app/src/main/java/com/fireblaze/evento/SnackBarContainerInterface.java
@@ -2,9 +2,6 @@ package com.fireblaze.evento;
 
 import android.view.View;
 
-/**
- * Created by shivam on 10/11/2016.
- */
 
 public interface SnackBarContainerInterface {
      View getContainer();
