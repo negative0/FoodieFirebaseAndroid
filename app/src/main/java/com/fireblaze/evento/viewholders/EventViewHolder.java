2d38d30479cadbbbb9fd2caffd13dca140743c00
Chaitanya <bchaitanya15@gmail.com>
2016-10-30 08:41:43 +0530

    New Event

---
+++ app/src/main/java/com/fireblaze/evento/viewholders/EventViewHolder.java
@@ -0,0 +1,10 @@
+package com.fireblaze.evento.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by fireblaze on 29/10/16.
+ */
+
+public class EventViewHolder extends RecyclerView.ViewHolder {
+}
